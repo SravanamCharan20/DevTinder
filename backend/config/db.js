4084851801c84f924419e979c17387eb1e5315a9
charan <sravanamcharan20@gmail.com>
2026-02-05 12:22:47 +0530

    Add .gitignore and configure dotenv for environment variables

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const connectDB = async () => {
-  await mongoose.connect(
-    "mongodb+srv://devtinder:devtinder@cluster0.8wdxlg0.mongodb.net/?appName=Cluster0"
-  );
+  await mongoose.connect(process.env.MONGO_URL);
 };
 
 export default connectDB;
