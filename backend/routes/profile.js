11916e5ce5179fe045a6c2b4b51c7a35a05f8951
charan <sravanamcharan20@gmail.com>
2026-02-06 15:45:31 +0530

    Add password reset functionality and improve profile update error handling

--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -18,19 +18,41 @@ profileRouter.patch("/edit", userAuth, async (req, res) => {
   try {
     const user = req.user;
     if (!profileEditValidator(req)) {
-        return res.status(400).json({ message: "Invalid fields for update" });
+      return res.status(400).json({ message: "Invalid fields for update" });
     }
 
     Object.keys(req.body).forEach((key) => (user[key] = req.body[key]));
-    await user.save()
+    await user.save();
     const userData = user.toObject();
     delete userData.password;
-    delete userData.email;    
-    
+    delete userData.email;
+
     res.json({
-        message: `${user.firstName}, your profile updated successfuly`,
-        data: userData
-      });
+      message: `${user.firstName}, your profile updated successfuly`,
+      data: userData,
+    });
+  } catch (error) {
+    res.send("Error : " + error.message);
+  }
+});
+
+profileRouter.patch("/forgetPassword", userAuth, async (req, res) => {
+  try {
+    const { newPass, confNewPass } = req.body;
+
+    if (!newPass || !confNewPass) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    if (newPass !== confNewPass) {
+      return res.status(400).json({ message: "Passwords do not match" });
+    }
+
+    const loggedInUser = req.user;
+    loggedInUser.password = newPass;
+
+    await loggedInUser.save();
+    res.status(200).json({ message: "Password changed successfully!!" });
   } catch (error) {
     res.send("Error : " + error.message);
   }
