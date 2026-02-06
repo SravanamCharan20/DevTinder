160d0390c86314c7e3d07cbb9f42904cd7f728bb
charan <sravanamcharan20@gmail.com>
2026-02-06 16:30:40 +0530

    Add validation to prevent self-connection requests and improve error handling in request routing

--- backend/models/connectionRequest.js
+++ backend/models/connectionRequest.js
@@ -25,4 +25,14 @@ const connectionRequestSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+connectionRequestSchema.pre("save", function (next) {
+  const connectionRequest = this;
+
+  if (connectionRequest.fromUserId.equals(connectionRequest.toUserId)) {
+    throw new Error("You cannot send a connection request to yourself Bro!!");
+  }
+
+  next();
+});
+
 export default mongoose.model("connectionRequest", connectionRequestSchema);
